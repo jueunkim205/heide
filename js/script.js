9e00e3d97689cb800e13bb5649df7b1f1ca43f51
jueunkim205 <monolinum@naver.com>
2025-05-15 22:23:22 +0900

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-$(function(){
+$(document).ready(function () {
+  console.log("JS 연결됨");
 
+  $('nav').mouseenter(function () {
+    console.log("nav에 마우스 올라감");
+    $('.subMenuWrap').stop().slideDown(500);
+  });
 
-})
+  $('nav').mouseleave(function () {
+    console.log("nav에서 마우스 나감");
+    $('.subMenuWrap').stop().slideUp(500);
+  });
+});
